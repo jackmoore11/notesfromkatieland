29082e257cfa674579f475411c0458145ca01014
Jack Moore <jackmoore11@icloud.com>
2023-10-25 20:17:44 -0700

    Script to add allowed users, name column added to AllowedUser

---
+++ AddAllowedUsers.py
@@ -0,0 +1,25 @@
+import json
+from notesfromkatieland import createApp, db
+from notesfromkatieland.models import AllowedUser
+
+app = createApp()
+app.app_context().push()
+db.create_all()
+
+currentAllowedUsers = AllowedUser.query.all()
+currentEmails = []
+for allowedUser in currentAllowedUsers:
+    currentEmails.append(allowedUser.email)
+
+f = open('AllowedUsers.json')
+userData = json.load(f)
+f.close()
+
+for name, email in userData.items():
+    if email not in currentEmails:
+        allowedUser = AllowedUser(name=name, email=email)
+        db.session.add(allowedUser)
+    else:
+        print(f'{name} already in database')
+
+db.session.commit()
--- notesfromkatieland/models.py
+++ notesfromkatieland/models.py
@@ -44,7 +44,8 @@ class Post(db.Model):
     
 class AllowedUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
 
     def __repr__(self):
-        return f"AllowedUser('{self.email}')"
+        return f"AllowedUser('{self.name}', '{self.email}')"
